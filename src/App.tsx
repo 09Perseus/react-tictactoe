2ebbc19439ca2af94600715820e6278bde5df8b2
09Perseus <bkumarevel@gmail.com>
2026-03-09 13:29:36 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,107 @@
+import { useState } from 'react'
+import './App.css'
+
+//Square component
+function Square({value, onSquareClick}): TSX.Element{
+
+  return (
+    <button className="square" onClick={onSquareClick}> 
+      {value} 
+    </button>
+  );
+}
+
+
+//function that displays the board
+function Board(): TSX.Element {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(i){
+    if (squares[i] || calculateWinner(squares)){
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if (xIsNext){
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  
+  //Checks for winner
+  function calculateWinner(squares) {
+
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+       }
+    }
+
+    return null
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+
+  } else if (!squares.some(element => element === null)) {
+    status = "Tie"
+
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
+
+  return (
+    <>
+      <div>
+        {status}
+      </div>
+      <div>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() =>handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  )
+}
+
+//default game function
+export default function Game() {
+  return (
+    <div>
+      <div>
+        <Board />
+      </div>
+      <div>
+        <ol></ol>
+      </div>
+    </div>
+  )
+}
